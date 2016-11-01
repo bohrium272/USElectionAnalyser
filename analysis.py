baeac60393db7a61a0388fe90e6451bb71447e45
Arpit Gogia <arpitgogia@protonmail.com>
2016-11-01 12:49:49 +0530

    Ready to fetch tweets

--- analysis.py
+++ analysis.py
@@ -1,7 +1,8 @@
-from twitter import TwitterStream, OAuth
+from twitter import TwitterStream, OAuth, TwitterHTTPError, BadStatusLine, URLError, SSLError, socket.error
 from os import environ
 import json
 import pandas
+import time
 
 tweets = []
     
@@ -9,16 +10,26 @@ def fetch_tweets(collection, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CO
     print "Inside fetch_tweets"
     tweet_stream = TwitterStream(auth=OAuth(ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_KEY_SECRET))
     print "Constructed Stream"
-    iterator = tweet_stream.statuses.filter(track='USElections')
-    print "Iterator Obtained"
     i = 0
-    for tweet in iterator:
-        if i == 1:
-            break
-        print json.dumps(tweet)
-        collection.insert_one(tweet)
-        i += 1 
-    print "Iteration Finished"
+    while True:
+        try:
+            for tweet in tweet_stream.statuses.filter(track='USElections'):
+                if not tweet or tweet.get("timeout"):
+                        continue
+                if tweet.get("disconnect") or tweet.get("hangup"):
+                    print("WARNING Stream connection lost: %s" % msg)
+                    break
+                if i == 12000:
+                    break
+                if tweet.get('text'):
+                    collection.insert_one(tweet)
+                    i += 1
+                    if i % 1000 == 0:
+                        print "Completed " + str(i) + " iterations"
+         except(TwitterHTTPError, BadStatusLine, URLError, SSLError, socket.error) as e:
+            print "WARNING: Stream connection lost, reconnecting in a sec... (%s: %s)" % (type(e), e):
+            time.sleep(1)
+    print "Fetch Finished"
 
 def create_data_frame(collection):
     tweets_db = collection.find()
